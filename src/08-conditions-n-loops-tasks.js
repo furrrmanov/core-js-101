1b0f2fc815b5f88441bf44de2838d72dc5563fa2
furrrmanov <pokerzatashil@mail.ru>
2020-05-01 15:45:00 +0300

    feat: add solution

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -219,8 +219,8 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 /**
@@ -235,8 +235,9 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  const x = String(num);
+  return Number(x.split('').reverse().join(''));
 }
 
 /**
@@ -259,8 +260,19 @@ function reverseInteger(/* num */) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  let bool;
+  const num = String(ccn).split('');
+  const array = [];
+  const x = num.reverse().map((it, index) => (index % 2 !== 0 ? +it * 2 : +it));
+  x.map((it) => (it > 9 ? array.push(it - 9) : array.push(it)));
+  const sum = array.reduce((a, b) => a + b);
+  if (sum % 10 === 0) {
+    bool = true;
+  } else {
+    bool = false;
+  }
+  return bool;
 }
 
 /**
@@ -277,8 +289,15 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const arr = String(num).split('');
+  const sum = arr.reduce((a, b) => +a + +b);
+  let res = 1;
+  if (sum > 9) {
+    const x = String(sum).split('');
+    res = x.reduce((a, b) => +a + +b);
+  }
+  return res;
 }
 
 /**
